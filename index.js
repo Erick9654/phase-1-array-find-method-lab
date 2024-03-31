63753c445edd974290da6cc80b166df3b4b8d941
Erick Koome <koomembwiria@gmail.com>
2024-03-31 19:55:15 +0300

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const testVar = {}
+
+function testFunc() {
+  return "hi"
+}
+
+function superbowlWin(array) {
+  const win = array.find(game => game.result === "W")
+  return win ? win.year : undefined 
+}
